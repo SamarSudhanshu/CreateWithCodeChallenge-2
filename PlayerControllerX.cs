e77c4a52e384771fb0cfdf32c9cc1e75dbd44ed2
Samar Sudhanshu <95526300+SamarSudhanshu@users.noreply.github.com>
2023-09-05 19:52:49 +0530

    Add files via upload

---
+++ PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float intervalDelay = 2.0f;
+    private float lastSpawnTime;
+
+    void Start()
+    {
+        lastSpawnTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - lastSpawnTime >= intervalDelay)
+        {
+            SpawnDog();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    void SpawnDog()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+}
